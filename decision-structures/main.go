d25144547f1b8fb99921b548ee823bd8e4daf9e0
grbalmeida <g.r.almeida@live.com>
2023-09-20 09:34:01 -0300

    Decision structures

---
+++ decision-structures/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "log"
+
+func main() {
+	var isTrue bool
+
+	isTrue = false
+
+	if isTrue {
+		log.Println("isTrue is", isTrue)
+	} else {
+		log.Println("isTrue is", isTrue)
+	}
+
+	cat := "cat2"
+
+	if cat == "cat" {
+		log.Println("Cat is cat")
+	} else {
+		log.Println("Cat is not cat")
+	}
+
+	myNum := 100
+	isTrue = false
+
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is greather than 99 and isTrue is set to true")
+	} else if myNum < 100 && isTrue {
+		log.Println("1")
+	} else if myNum == 101 || isTrue {
+		log.Println("2")
+	} else if myNum > 1000 && isTrue == false {
+		log.Println("3")
+	}
+
+	myVar := "horse"
+
+	switch myVar {
+	case "cat":
+		log.Println("myVar is set to cat")
+	case "dog":
+		log.Println("myVar is set to dog")
+	case "fish":
+		log.Println("myVar is set to fish")
+	default:
+		log.Println("myVar is something else")
+	}
+}
