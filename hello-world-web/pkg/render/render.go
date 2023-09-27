6d4fae042c96c99cac7f1f1ef02b0a28d3a4985f
grbalmeida <g.r.almeida@live.com>
2023-09-27 09:04:21 -0300

    Building a simple template cache

--- hello-world-web/pkg/render/render.go
+++ hello-world-web/pkg/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
@@ -14,4 +15,54 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		fmt.Println("error parsing template:", err)
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check to see if we already have the template in our cache
+	_, inMap := tc[t]
+
+	if !inMap {
+		// need to create the template
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// we have the template in the cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s.html", t),
+		"./templates/base.layout.tmpl.html",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	// add template to cache (map)
+	tc[t] = tmpl
+
+	return nil
 }
